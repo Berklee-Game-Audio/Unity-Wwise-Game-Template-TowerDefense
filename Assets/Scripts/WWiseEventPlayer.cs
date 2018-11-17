e80f6031a5e26cd6746620d9acd459c3fffc1d20
Michael Sweet <msweet37@gmail.com>
2018-11-17 15:20:48 -0500

    initial commit

---
+++ Assets/Scripts/WWiseEventPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WWiseEventPlayer : MonoBehaviour {
+
+    public string WwiseEventToPlay = "";
+    static private GameObject WwiseGlobal;
+
+    private void Awake()
+    {
+        WwiseGlobal = GameObject.Find("WwiseGlobal");
+    }
+
+    public void PlayWwiseEvent(string WwiseEventOverride = ""){
+        if(WwiseGlobal == null){
+            Debug.Log("WwiseGlobal not found.");
+            return;
+        }
+
+        if (WwiseEventOverride != "")
+        {
+            AkSoundEngine.PostEvent(WwiseEventOverride, WwiseGlobal);
+            return;
+        }
+
+
+        if (WwiseEventToPlay != "")
+        {
+            AkSoundEngine.PostEvent(WwiseEventToPlay, WwiseGlobal);
+        }
+    }
+}
---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,57 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID GAME_LEVELLOSE = 184505919U;
+        static const AkUniqueID GAME_LEVELWIN = 3058981574U;
+        static const AkUniqueID GAME_MENU = 636067499U;
+        static const AkUniqueID GAME_START = 733168346U;
+        static const AkUniqueID UI_BACKBUTTON = 1593593619U;
+        static const AkUniqueID UI_LEVELSELECTBUTTON = 3680374304U;
+        static const AkUniqueID UI_LEVELSELECTBUTTONPART2 = 1013525255U;
+        static const AkUniqueID UI_OBJECTATTACKSOUND = 1688641372U;
+        static const AkUniqueID UI_OBJECTCHARGESOUND = 2611829248U;
+        static const AkUniqueID UI_OBJECTDEATH = 2799761671U;
+        static const AkUniqueID UI_OBJECTFIREPROJECTILE = 3377525770U;
+        static const AkUniqueID UI_OBJECTSELLBUTTON = 1843602067U;
+        static const AkUniqueID UI_OPTIONSSELECTBUTTON = 848228196U;
+        static const AkUniqueID UI_QUITSELECTBUTTON = 2671938355U;
+        static const AkUniqueID UI_STARTWAVEBUTTON = 1726823933U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID HEALTH = 3677180323U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID TOWER_DEFENSE_SOUNDBANK = 2295945583U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
