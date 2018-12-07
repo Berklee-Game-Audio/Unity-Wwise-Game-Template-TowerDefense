7de7ecd0dc5afeae393b2da0afddc086f658f3a8
Michael Sweet <msweet37@gmail.com>
2018-12-06 21:49:58 -0500

    class changes

---
+++ Unity-Tower-Defense-Game-Kit_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,107 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID GAME_LEVELLOSE = 184505919U;
+        static const AkUniqueID GAME_LEVELWIN = 3058981574U;
+        static const AkUniqueID GAME_MENU = 636067499U;
+        static const AkUniqueID GAME_START = 733168346U;
+        static const AkUniqueID UI_LEVELSELECTBUTTON = 3680374304U;
+        static const AkUniqueID UI_LEVELSELECTBUTTONPART2 = 1013525255U;
+        static const AkUniqueID UI_NEWWAVE = 1043510291U;
+        static const AkUniqueID UI_STARTWAVEBUTTON = 1726823933U;
+        static const AkUniqueID UI_TESTEVENT = 170582342U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAME_STATE
+        {
+            static const AkUniqueID GROUP = 766723505U;
+
+            namespace STATE
+            {
+                static const AkUniqueID GAMEPLAY = 89505537U;
+                static const AkUniqueID LOSE = 221232726U;
+                static const AkUniqueID MENU = 2607556080U;
+                static const AkUniqueID WIN = 979765101U;
+            } // namespace STATE
+        } // namespace GAME_STATE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace LEVEL_INTENSITY
+        {
+            static const AkUniqueID GROUP = 1353422267U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID INTENSITY_0 = 3700547913U;
+                static const AkUniqueID INTENSITY_1 = 3700547912U;
+                static const AkUniqueID INTENSITY_2 = 3700547915U;
+                static const AkUniqueID INTENSITY_3 = 3700547914U;
+                static const AkUniqueID INTENSITY_4 = 3700547917U;
+                static const AkUniqueID INTENSITY_5 = 3700547916U;
+                static const AkUniqueID INTENSITY_6 = 3700547919U;
+                static const AkUniqueID INTENSITY_7 = 3700547918U;
+                static const AkUniqueID INTENSITY_8 = 3700547905U;
+                static const AkUniqueID INTENSITY_9 = 3700547904U;
+                static const AkUniqueID INTENSITY_10 = 2175116392U;
+            } // namespace SWITCH
+        } // namespace LEVEL_INTENSITY
+
+        namespace LEVEL_INTENSITY_ALT
+        {
+            static const AkUniqueID GROUP = 3133676099U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID INTENSITY_0 = 3700547913U;
+                static const AkUniqueID INTENSITY_1 = 3700547912U;
+                static const AkUniqueID INTENSITY_2 = 3700547915U;
+                static const AkUniqueID INTENSITY_3 = 3700547914U;
+            } // namespace SWITCH
+        } // namespace LEVEL_INTENSITY_ALT
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID HEALTH = 3677180323U;
+        static const AkUniqueID LEVELNUMBER = 3941060586U;
+        static const AkUniqueID WAVENUMBER = 3188605873U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID TOWER_DEFENSE_SOUNDBANK = 2295945583U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
