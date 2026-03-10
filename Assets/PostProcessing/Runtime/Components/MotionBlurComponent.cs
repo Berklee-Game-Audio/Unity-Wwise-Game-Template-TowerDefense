69e4873ab87efbfb2d148bf1d8bf7f2e2fe2870f
Michael Sweet <msweet37@gmail.com>
2026-03-10 14:08:34 -0700

    small bug fixes

--- Assets/PostProcessing/Runtime/Components/MotionBlurComponent.cs
+++ Assets/PostProcessing/Runtime/Components/MotionBlurComponent.cs
@@ -347,7 +347,6 @@ namespace UnityEngine.PostProcessing
                 var settings = model.settings;
                 return model.enabled
                        && ((settings.shutterAngle > 0f && reconstructionFilter.IsSupported()) || settings.frameBlending > 0f)
-                       && SystemInfo.graphicsDeviceType != GraphicsDeviceType.OpenGLES2 // No movecs on GLES2 platforms
                        && !context.interrupted;
             }
         }
